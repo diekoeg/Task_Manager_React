5d4bac11e9d31d811e5a206aea3770e19af198fa
Diego Estrada <diego_estrada14@hotmail.com>
2023-08-12 07:09:08 -0600

    Ya se puso el contenedor para las funcionalidades secundarias para obsevar el progreso y demás, falta crear la navbar, así como las demas funcionalidades

--- src/App/AppUI.jsx
+++ src/App/AppUI.jsx
@@ -10,15 +10,12 @@ import { EmptyTodos } from '../EmptyTodos';
 import { TodoContext } from '../TodoContext';
 import { Modal } from '../Modal';
 import { TodoForm } from '../TodoForm';
+import { Navbar } from '../Navbar';
 
 /* import { CompletedContainer } from './CompletedContainer'; */
 
 import './main.css';
 import React from 'react';
-
-
-
-
 
 function AppUI(){
   const {
@@ -61,10 +58,17 @@ function AppUI(){
                 showModal = {() => showModal()}
               />
             }
-            
           </div>
+          <div className='rightContainer'>
+            <Navbar />
+{/*             {navbarState === 1 && <Progress />}
+            {navbarState === 2 && <CompletedList />}
+            {navbarState === 3 && <Timer />} */}
+          </div>
+
         </div>
       </div>
+
 
       {openModal && (
         <Modal>
